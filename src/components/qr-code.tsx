c696dc1a685038b2342db2f56e7e9a968c033b3b
= <=>
2024-01-05 18:23:17 -0500

    create qr code component skeleton

---
+++ src/components/qr-code.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const qrCode = () => {
+  return (
+    <div>
+      <h1>QR Code</h1>
+    </div>
+  );
+};
+
+export default qrCode;
---
+++ src/components/test.tsx
@@ -0,0 +1,12 @@
+// create a test component
+// Path: src/components/test.tsx
+
+const test = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default test;
