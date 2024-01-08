a041e92e38fcebacd46650300455fb6a94d878a6
= <=>
2024-01-08 18:41:55 -0500

    corrects based on feedback from community

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import Qrcode from "./components/qr-code";
 
 function App() {
   return (
-    <div className="App h-screen">
-      <div className="flex justify-center items-center h-full">
+    <div className="App">
+      <div className="flex justify-center items-center flex-col h-screen">
         {<Qrcode />}
+        <div className="attribution">
+          Challenge by
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Mokopolo</a>.
+        </div>
       </div>
     </div>
   );
--- src/components/qr-code.tsx
+++ src/components/qr-code.tsx
@@ -11,19 +11,16 @@ const qrCode = () => {
           alt="QR Code"
         />
       </div>
-      <div
-        className="font-bold text-center my-4 leading-6"
-        style={{ fontSize: "22px" }}
-      >
+      <h1 className="font-bold text-center my-4 leading-6 text-[22px]">
         Improve your front-end skills by building projects
-      </div>
-      <div
-        className="text-center leading-5"
-        style={{ color: "hsl(220, 15%, 55%)", fontSize: 15 }}
+      </h1>
+      <p
+        className="text-center leading-5 text-[15px]"
+        style={{ color: "hsl(220, 15%, 55%)" }}
       >
         Scan the QR code to visit Frontend Mentor and take your coding skills to
         the next level.
-      </div>
+      </p>
     </div>
   );
 };
